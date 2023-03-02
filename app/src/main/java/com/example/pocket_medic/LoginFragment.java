f837b622a7f2e20380fdcd989535289b64190fab
OsvaldoPerezuwu <l19141186@queretaro.tecnm.mx>
2023-03-02 00:29:52 -0600

    Initial commit

---
+++ app/src/main/java/com/example/pocket_medic/LoginFragment.java
@@ -0,0 +1,40 @@
+package com.example.pocket_medic;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginFragment extends Fragment {
+
+    private TextView txt_regis;
+    private EditText txt_usuario,txt_password;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View vista = inflater.inflate(R.layout.fragment_login, container, false);
+
+        txt_regis = (TextView)vista.findViewById(R.id.lbl_regis);
+        txt_usuario = (EditText)vista.findViewById(R.id.edtUsuario);
+        txt_password = (EditText)vista.findViewById(R.id.edtPassword);
+
+
+        //Hacemos que un edittext sirva como un boton que nos lleve a un formulario de registro
+        txt_regis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+            }
+        });
+
+        return vista;
+    }
+}
---
+++ app/src/main/java/com/example/pocket_medic/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.pocket_medic;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    private DrawerLayout drawerLayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.abrir_nav, R.string.cerrar_nav);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if(savedInstanceState == null)
+        {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new InicioFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_inicio);
+        }
+    }
+    public boolean onNavigationItemSelected(@NonNull MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.nav_inicio:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new InicioFragment()).commit();
+                break;
+
+            case R.id.nav_citas:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new CitasFragment()).commit();
+                break;
+
+            case R.id.nav_telefonos:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new TelefonosFragment()).commit();
+                break;
+
+            case R.id.nav_guias:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new GuiasFragment()).commit();
+                break;
+
+            case R.id.nav_recetas:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new RecetasFragment()).commit();
+                break;
+
+            case R.id.nav_usuario:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new UsuarioFragment()).commit();
+                break;
+
+            case R.id.nav_direcciones:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new DireccionesFragment()).commit();
+                break;
+
+            case R.id.nav_login:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new LoginFragment()).commit();
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
