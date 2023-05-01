b339e5146044d3c27ee2c33fdf80e66aa2e46eaf
KevanAvellaneda <l19141120@queretaro.tecnm.mx>
2023-05-01 15:27:20 -0600

    Titulos de las interfaces (provisional) y su respectivo logo

--- app/src/main/java/com/example/pocket_medic/GuiasFragment.java
+++ app/src/main/java/com/example/pocket_medic/GuiasFragment.java
@@ -2,6 +2,7 @@ package com.example.pocket_medic;
 
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -12,10 +13,13 @@ import android.view.ViewGroup;
 public class GuiasFragment extends Fragment {
 
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_guias, container, false);
+
+
     }
 }
