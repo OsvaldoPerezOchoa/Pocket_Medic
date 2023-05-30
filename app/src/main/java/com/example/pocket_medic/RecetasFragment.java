6c065d79d0c0a265b0c90edc9694eb777a856bf4
D-Uribe <d.uribesandoval@gmail.com>
2023-05-29 22:35:59 -0600

    agrego interfaces

--- app/src/main/java/com/example/pocket_medic/RecetasFragment.java
+++ app/src/main/java/com/example/pocket_medic/RecetasFragment.java
@@ -7,13 +7,31 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class RecetasFragment extends Fragment {
+    public static final String EXTRA_MESSAGE = "com.example.notes.MESSAGE";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_recetas, container, false);
     }
 }
