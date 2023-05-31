cfbc78bac285537347f5aa3ab4e408dff202ae7e
D-Uribe <d.uribesandoval@gmail.com>
2023-05-30 21:57:17 -0600

    pruebas receta y cita

---
+++ app/src/main/java/com/example/pocket_medic/CreacitaFragment.java
@@ -0,0 +1,19 @@
+package com.example.pocket_medic;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class CreacitaFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_creacita, container, false);
+    }
+}
